f84b8c53223c59db9e11615f0cee7f89de7d508d
jjspscl <jjspscl@gmail.com>
2024-04-06 16:58:29 +0800

    REMOVE frontend

--- frontend/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
-
-import vue from "@astrojs/vue";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [sitemap(), vue()]
-});
--- frontend/src/api/blogs.ts
+++
@@ -1,6 +0,0 @@
-import instance from '.';
-export const getBlogs = async () => {
-    const response = await instance.get('https://swapi.dev/api/people')
-    const data = response?.data;
-    return data?.results as any[];
-}
