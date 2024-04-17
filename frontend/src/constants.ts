c4755a093e8feb4f749e61226834b8632f2d4877
jjspscl <jjspscl@gmail.com>
2024-04-18 00:17:19 +0800

    JS-5 | RSS Sitemap

---
+++ frontend/src/constants.ts
@@ -0,0 +1,12 @@
+// Data defined in Storyblok at 
+
+import { useStoryblokApi } from '@storyblok/astro';
+const storyblokApi = useStoryblokApi();
+
+const { data } = await storyblokApi.get(`cdn/datasource_entries`, {
+	datasource: 'site-consts'
+});
+
+////////////////////////////////////////////////////////////////
+export const SITE_TITLE = data.datasource_entries.filter((object) => object.name === 'SITE_TITLE')[0].value;
+export const SITE_DESCRIPTION = data.datasource_entries.filter((object) => object.name === 'SITE_DESCRIPTION')[0].value;
