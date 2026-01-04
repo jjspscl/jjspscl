d9f10422f9a1c141c83d206b8cdf501abae87062
jjspscl <jjspscl@gmail.com>
2026-01-04 21:30:06 +0800

    🔧 Add repository link support to ProjectCard component

--- frontend/src/features/projects/project.type.ts
+++ frontend/src/features/projects/project.type.ts
@@ -1,5 +1,13 @@
 import type { ITagAsset } from "@lib/tag";
 
+export interface IStoryblokLink {
+    id: string;
+    url: string;
+    linktype: "url" | "story" | "email" | "asset";
+    fieldtype: "multilink";
+    cached_url: string;
+}
+
 export interface ITechTag {
     _uid: string;
     name: string;
@@ -17,5 +25,6 @@ export interface IProject {
     title: string;
     headline: string;
     demo_url: string;
+    repository: IStoryblokLink;
     technology: ITechTagResolved[];
 }
